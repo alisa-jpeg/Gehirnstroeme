aaf35c568f31ee5760c2cd8f77b78b59e12e338e
Katharina <katharina.ensslen@outlook.de>
2025-02-18 13:49:33 +0100

    Update durchschnitt_und_bilder_anzeigen.py
    Signed-off-by: Katharina <katharina.ensslen@outlook.de>

--- durchschnitt_und_bilder_anzeigen.py
+++ durchschnitt_und_bilder_anzeigen.py
@@ -7,7 +7,7 @@ import socket
 #importieren und initialisieren von pygame
 import pygame
 
-import calculate_average_alpha)
+from durchschnitt_berechnen import calculate_average_alpha
 
 pygame.init()
 
